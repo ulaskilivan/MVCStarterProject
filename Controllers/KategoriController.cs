602841410a45655ad584ce743b9f69f2be55405d
Ulaş <ulas.klvn@gmail.com>
2024-04-12 10:55:55 +0300

    Navbar Created & Categories Added

---
+++ Controllers/KategoriController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCStarter.Models.Entity;
+
+namespace MVCStarter.Controllers
+{
+    public class KategoriController : Controller
+    {
+        // GET: Kategori
+        MvcDBStokEntities db=new MvcDBStokEntities();
+        public ActionResult Index()
+        {
+            var values = db.TBLKATEGORILERs.ToList();
+            return View(values);
+        }
+    }
+}
