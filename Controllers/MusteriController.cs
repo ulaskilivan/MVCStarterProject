1e94a40141e5403e99d5de1722f097bf613b6806
Ulaş <ulas.klvn@gmail.com>
2024-04-12 12:22:47 +0300

    Listing All DB Info

---
+++ Controllers/MusteriController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCStarter.Models.Entity;
+namespace MVCStarter.Controllers
+{
+    public class MusteriController : Controller
+    {
+        // GET: Musteri
+        MvcDBStokEntities db = new MvcDBStokEntities();
+
+        public ActionResult Index()
+        {
+            var values = db.TBLMUSTERILERs.ToList();
+            return View(values);
+        }
+    }
+}
---
+++ Controllers/UrunController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCStarter.Models.Entity;
+namespace MVCStarter.Controllers
+{
+    public class UrunController : Controller
+    {
+        // GET: Urun
+        MvcDBStokEntities db = new MvcDBStokEntities();
+        public ActionResult Index()
+        {
+            var values = db.TBLURUNLERs.ToList();
+            return View(values);
+        }
+    }
+}
